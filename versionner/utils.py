8e9a5ceabdb6175502ea538a542f8dbb81763489
Marcin Sztolcman <marcin@urzenia.net>
2015-05-14 15:53:01 +0200

    refined message about python3

--- versionner/utils.py
+++ versionner/utils.py
@@ -12,5 +12,5 @@ def validate_python_version():
     Validate python interpreter version. Only 3.3+ allowed.
     """
     if pkg_resources.parse_version(platform.python_version()) < pkg_resources.parse_version('3.3.0'):
-        print("Sorry, Python 3 is required")
+        print("Sorry, Python 3.3+ is required")
         sys.exit(1)
