1668ec2110edd4513c15d056353625510bdf7043
Marcin Sztolcman <marcin@urzenia.net>
2015-05-14 15:07:05 +0200

    pep8 refinements (pylint)

--- versionner/utils.py
+++ versionner/utils.py
@@ -1,8 +1,16 @@
+"""
+    Additional utilities
+"""
+
 import pkg_resources
 import platform
 import sys
 
+
 def validate_python_version():
+    """
+    Validate python interpreter version. Only 3.3+ allowed.
+    """
     if pkg_resources.parse_version(platform.python_version()) < pkg_resources.parse_version('3.3.0'):
         print("Sorry, Python 3 is required")
         sys.exit(1)
