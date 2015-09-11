dea6c84dfa50b4152580369e31be02bad0850d7e
Marcin Sztolcman <marcin@urzenia.net>
2015-09-11 13:20:34 +0200

    added abstraction layer for VCS

--- versionner/vcs/__init__.py
+++ versionner/vcs/__init__.py
@@ -1,106 +1,27 @@
 import importlib
-import subprocess
 
-from versionner import defaults
 from versionner.vcs import errors
 
 
 class VCS:
-    """
-        Main class for working with VCS
-    """
-
     def __init__(self, engine):
-        """
-        Initializer
-
-        :param engine:
-        :return:
-        """
         self._engine = engine
 
         try:
-            builder = importlib.import_module(engine, 'vcs')
+            builder = importlib.import_module('versionner.vcs.%s' % engine)
         except ImportError:
             raise errors.UnknownVCSError("Unknown VCS engine: %s" % engine)
 
-        self._command = builder.VCSCommandsBuilder()
-
-    @staticmethod
-    def _exec(cmd):
-        """
-        Execute command using subprocess.Popen
-        :param cmd:
-        :return: (code, stdout, stderr)
-        """
-        process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-
-        # pylint: disable=unexpected-keyword-arg
-        (stdout, stderr) = process.communicate(timeout=defaults.DEFAULT_VCS_TIMEOUT)
-
-        return process.returncode, stdout.decode(), stderr.decode()
+        self._command = builder.VCSEngine()
 
     def create_tag(self, version, params):
-        """
-        Run VCS command for tag using subprocess.Popen
-
-        :param version:
-        :param params:
-        :return:
-        """
-        cmd = self._command.tag(version, params)
-
-        (code, stdout, stderr) = self._exec(cmd)
-
-        if code:
-            raise errors.VCSError('Can\'t create VCS tag %s. Process exited with code %d and message: %s' % (
-                version, code, stderr or stdout))
+        return self._command.create_tag(version, params)
 
     def raise_if_cant_commit(self):
-        """
-        Verify VCS status and raise an error if commit is disallowed
-
-        :return:
-        """
-        cmd = self._command.status()
-
-        (code, stdout, stderr) = self._exec(cmd)
-
-        if code:
-            raise errors.VCSError('Can\'t verify VCS status. Process exited with code %d and message: %s' % (
-                code, stderr or stdout))
-
-        for line in stdout.splitlines():
-            if line.startswith(('??', '!!')):
-                continue
-            raise errors.VCSStateError("VCS status doesn't allow to commit. Please commit or stash your changes and try again")
+        return self._command.raise_if_cant_commit()
 
     def create_commit(self, message):
-        """
-        Create commit
-
-        :param message:
-        :return:
-        """
-        cmd = self._command.commit(message)
-
-        (code, stdout, stderr) = self._exec(cmd)
-
-        if code:
-            raise errors.VCSError('Commit failed. Process exited with code %d and message: %s' % (
-                code, stderr or stdout))
+        return self._command.create_tag(message)
 
     def add_to_stage(self, paths):
-        """
-        Stage given files
-
-        :param paths:
-        :return:
-        """
-        cmd = self._command.add(paths)
-
-        (code, stdout, stderr) = self._exec(cmd)
-
-        if code:
-            raise errors.VCSError('Can\'t add paths to VCS. Process exited with code %d and message: %s' % (
-                code, stderr + stdout))
+        return self._command.add_to_stage(paths)
--- versionner/vcs/git.py
+++ versionner/vcs/git.py
@@ -1,10 +1,15 @@
+import subprocess
+
+from versionner import defaults
+from versionner.vcs import errors
+
+
 class VCSCommandsBuilder:
     """ Build shell VCS command
     """
-    def __init__(self, engine):
-        self._engine = engine
 
-    def tag(self, version, params):
+    @staticmethod
+    def tag(version, params):
         """
         Build and return full command to use with subprocess.Popen for 'git tag' command
 
@@ -12,66 +17,131 @@ class VCSCommandsBuilder:
         :param params:
         :return: list
         """
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'tag', '-a', '-m', 'v%s' % version, str(version)]
-            if params:
-                cmd.extend(params)
-
-        if not cmd:
-            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+        cmd = ['git', 'tag', '-a', '-m', 'v%s' % version, str(version)]
+        if params:
+            cmd.extend(params)
 
         return cmd
 
-    def status(self):
+    @staticmethod
+    def status():
         """
         Build and return full command to use with subprocess.Popen for 'git status' command
 
         :return: list
         """
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'status', '--porcelain']
-
-        if not cmd:
-            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+        cmd = ['git', 'status', '--porcelain']
 
         return cmd
 
-    def commit(self, message):
+    @staticmethod
+    def commit(message):
         """
         Build and return full command to use with subprocess.Popen for 'git commit' command
 
         :param message:
         :return: list
         """
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'commit', '-m', message]
-
-        if not cmd:
-            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+        cmd = ['git', 'commit', '-m', message]
 
         return cmd
 
-    def add(self, paths):
+    @staticmethod
+    def add(paths):
         """
         Build and return full command to use with subprocess.Popen for 'git add' command
 
         :param paths:
         :return: list
         """
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'add'] + list(paths)
-
-        if not cmd:
-            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+        cmd = ['git', 'add'] + list(paths)
 
         return cmd
 
 
+class VCSEngine:
+    """
+        Main class for working with VCS
+    """
+
+    def __init__(self):
+        self._command = VCSCommandsBuilder()
+
+    @staticmethod
+    def _exec(cmd):
+        """
+        Execute command using subprocess.Popen
+        :param cmd:
+        :return: (code, stdout, stderr)
+        """
+        process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        # pylint: disable=unexpected-keyword-arg
+        (stdout, stderr) = process.communicate(timeout=defaults.DEFAULT_VCS_TIMEOUT)
+
+        return process.returncode, stdout.decode(), stderr.decode()
+
+    def create_tag(self, version, params):
+        """
+        Run VCS command for tag using subprocess.Popen
+
+        :param version:
+        :param params:
+        :return:
+        """
+        cmd = self._command.tag(version, params)
+
+        (code, stdout, stderr) = self._exec(cmd)
+
+        if code:
+            raise errors.VCSError('Can\'t create VCS tag %s. Process exited with code %d and message: %s' % (
+                version, code, stderr or stdout))
+
+    def raise_if_cant_commit(self):
+        """
+        Verify VCS status and raise an error if commit is disallowed
+
+        :return:
+        """
+        cmd = self._command.status()
+
+        (code, stdout, stderr) = self._exec(cmd)
+
+        if code:
+            raise errors.VCSError('Can\'t verify VCS status. Process exited with code %d and message: %s' % (
+                code, stderr or stdout))
+
+        for line in stdout.splitlines():
+            if line.startswith(('??', '!!')):
+                continue
+            raise errors.VCSStateError("VCS status doesn't allow to commit. Please commit or stash your changes and try again")
+
+    def create_commit(self, message):
+        """
+        Create commit
+
+        :param message:
+        :return:
+        """
+        cmd = self._command.commit(message)
+
+        (code, stdout, stderr) = self._exec(cmd)
+
+        if code:
+            raise errors.VCSError('Commit failed. Process exited with code %d and message: %s' % (
+                code, stderr or stdout))
+
+    def add_to_stage(self, paths):
+        """
+        Stage given files
+
+        :param paths:
+        :return:
+        """
+        cmd = self._command.add(paths)
+
+        (code, stdout, stderr) = self._exec(cmd)
+
+        if code:
+            raise errors.VCSError('Can\'t add paths to VCS. Process exited with code %d and message: %s' % (
+                code, stderr + stdout))
