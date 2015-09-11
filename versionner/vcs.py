c11f9aaaa615381a882ecad222f9df2d6d506664
Marcin Sztolcman <marcin@urzenia.net>
2015-09-11 12:28:09 +0200

    VCS._get_*_command extracted to VCSCommandsBuilder, also cleanups with exceptions

--- versionner/vcs.py
+++ versionner/vcs.py
@@ -7,25 +7,19 @@ import subprocess
 from versionner import defaults
 
 
-class VCSError(RuntimeError): pass
+class VCSError(RuntimeError):
+    pass
 
 
-# pylint: disable=too-few-public-methods
-class VCS(object):
-    """
-        Main class for working with VCS
-    """
+class UnknownVCSError(VCSError):
+    pass
 
+
+class VCSCommandsBuilder:
     def __init__(self, engine):
-        """
-        Initializer, just save 'engine' option
-
-        :param engine:
-        :return:
-        """
         self._engine = engine
 
-    def _get_tag_command(self, version, params):
+    def tag(self, version, params):
         """
         Build and return full command to use with subprocess.Popen
 
@@ -41,9 +35,59 @@ class VCS(object):
                 cmd.extend(params)
 
         if not cmd:
-            raise RuntimeError("Unknown VCS engine: %s" % self._engine)
+            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
 
         return cmd
+
+    def status(self):
+        cmd = None
+
+        if self._engine == 'git':
+            cmd = ['git', 'status', '--porcelain']
+
+        if not cmd:
+            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+
+        return cmd
+
+    def commit(self, message):
+        cmd = None
+
+        if self._engine == 'git':
+            cmd = ['git', 'commit', '-m', message]
+
+        if not cmd:
+            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+
+        return cmd
+
+    def add(self, paths):
+        cmd = None
+
+        if self._engine == 'git':
+            cmd = ['git', 'add'] + list(paths)
+
+        if not cmd:
+            raise UnknownVCSError("Unknown VCS engine: %s" % self._engine)
+
+        return cmd
+
+
+# pylint: disable=too-few-public-methods
+class VCS:
+    """
+        Main class for working with VCS
+    """
+
+    def __init__(self, engine):
+        """
+        Initializer, just save 'engine' option
+
+        :param engine:
+        :return:
+        """
+        self._engine = engine
+        self._command = VCSCommandsBuilder(engine)
 
     def create_tag(self, version, params):
         """
@@ -53,7 +97,7 @@ class VCS(object):
         :param params:
         :return:
         """
-        cmd = self._get_tag_command(version, params)
+        cmd = self._command.tag(version, params)
 
         process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
@@ -61,29 +105,18 @@ class VCS(object):
         (_, stderr) = process.communicate(timeout=defaults.DEFAULT_TAG_TIMEOUT)
 
         if process.returncode:
-            raise RuntimeError('Can\'t create VCS tag %s. Process exited with code %d and message: %s' % (
+            raise VCSError('Can\'t create VCS tag %s. Process exited with code %d and message: %s' % (
                 version, process.returncode, stderr.decode('utf-8')))
 
-    def _get_status_command(self):
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'status', '--porcelain']
-
-        if not cmd:
-            raise RuntimeError("Unknown VCS engine: %s" % self._engine)
-
-        return cmd
-
     def raise_if_cant_commit(self):
-        cmd = self._get_status_command()
+        cmd = self._command.status()
 
         process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
         (stdout, stderr) = process.communicate(timeout=defaults.DEFAULT_TAG_TIMEOUT)
 
         if process.returncode:
-            raise RuntimeError('Can\'t verify VCS status. Process exited with code %d and message: %s' % (
+            raise VCSError('Can\'t verify VCS status. Process exited with code %d and message: %s' % (
                 process.returncode, stderr))
 
         for line in stdout.splitlines():
@@ -91,46 +124,24 @@ class VCS(object):
                 continue
             raise VCSError("VCS status doesn't allow to commit. Please commit or stash your changes and try again")
 
-    def _get_commit_command(self, message):
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'commit', '-m', message]
-
-        if not cmd:
-            raise RuntimeError("Unknown VCS engine: %s" % self._engine)
-
-        return cmd
-
     def create_commit(self, message):
-        cmd = self._get_commit_command(message)
+        cmd = self._command.commit(message)
 
         process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
         (stdout, stderr) = process.communicate(timeout=defaults.DEFAULT_TAG_TIMEOUT)
 
         if process.returncode:
-            raise RuntimeError('Commit failed. Process exited with code %d and message: %s' % (
+            raise VCSError('Commit failed. Process exited with code %d and message: %s' % (
                 process.returncode, stderr.decode('utf-8')))
 
-    def _get_add_command(self, paths):
-        cmd = None
-
-        if self._engine == 'git':
-            cmd = ['git', 'add'] + list(paths)
-
-        if not cmd:
-            raise RuntimeError("Unknown VCS engine: %s" % self._engine)
-
-        return cmd
-
     def add_to_stage(self, paths):
-        cmd = self._get_add_command(paths)
+        cmd = self._command.add(paths)
 
         process = subprocess.Popen(cmd, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
 
         (stdout, stderr) = process.communicate(timeout=defaults.DEFAULT_TAG_TIMEOUT)
 
         if process.returncode:
-            raise RuntimeError('Can\'t add paths to VCS. Process exited with code %d and message: %s' % (
+            raise VCSError('Can\'t add paths to VCS. Process exited with code %d and message: %s' % (
                 process.returncode, stderr))
