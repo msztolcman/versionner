de957d808a1ddb39ec1d1bc5aa62df6b7b9f29ca
Marcin Sztolcman <marcin@urzenia.net>
2016-10-18 21:42:14 +0200

    removed unused functions (moved to commands.files_management)

--- versionner/cli.py
+++ versionner/cli.py
@@ -10,16 +10,11 @@ utils.validate_python_version()
 import argparse
 import pathlib
 import os.path
-import re
-import shutil
 import sys
-import tempfile
-import time
 
 import versionner
 from versionner import config
 from versionner import defaults
-from versionner import vcs
 from versionner import commands
 
 # pylint: disable=wildcard-import
@@ -183,97 +178,6 @@ def parse_args(args, cfg):
     elif version_file_requirement == 'none':
         if cfg.version_file.exists():
             p.error("Version file \"%s\" already exists" % cfg.version_file)
-
-
-def update_project_files(cfg, proj_version):
-    """
-    Update version string in project files
-
-    :rtype : dict
-    :param cfg:project configuration
-    :param proj_version:current version
-    :return:dict :raise ValueError:
-    """
-    counters = {'files': 0, 'changes': 0}
-
-    for project_file in cfg.files:
-        if not project_file.file.exists():
-            print("File \"%s\" not found" % project_file.filename, file=sys.stderr)
-            continue
-
-        # prepare data
-        date_format = project_file.date_format or cfg.date_format
-
-        rxp = re.compile(project_file.search, project_file.search_flags)
-        replace = project_file.replace % {
-            "date": time.strftime(date_format),
-            "major": proj_version.major,
-            "minor": proj_version.minor,
-            "patch": proj_version.patch,
-            "prerelease": proj_version.prerelease,
-            "version": str(proj_version),
-            "build": proj_version.build,
-        }
-
-        # update project files
-        with \
-                project_file.file.open(mode="r", encoding=project_file.encoding) as fh_in, \
-                tempfile.NamedTemporaryFile(mode="w", encoding=project_file.encoding, delete=False) as fh_out:
-            if project_file.match == 'line':
-                changes = 0
-                for line in fh_in:
-                    (line, cnt) = rxp.subn(replace, line)
-                    if cnt:
-                        changes += cnt
-                    fh_out.write(line)
-
-                if changes:
-                    counters['files'] += 1
-                    counters['changes'] += changes
-
-            elif project_file.match == 'file':
-                data = fh_in.read()
-                (data, cnt) = rxp.subn(replace, data)
-                if cnt:
-                    counters['files'] += 1
-                    counters['changes'] += cnt
-                fh_out.write(data)
-
-            else:
-                raise ConfigError("Unknown match type: \"%s\"" % project_file.match)
-
-            fh_out.close()
-
-            shutil.copystat(project_file.filename, fh_out.name)
-            pathlib.Path(fh_out.name).rename(project_file.filename)
-
-    return counters
-
-
-def save_version_and_update_files(cfg, version_file, version_to_save):
-    """
-    Save version to version_file and commit changes if required
-
-    :param cfg:
-    :param version_file:
-    :param version_to_save:
-    :return:
-    """
-    with vcs.VCS(cfg.vcs_engine) as vcs_handler:
-        if cfg.commit:
-            vcs_handler.raise_if_cant_commit()
-
-        version_file.write(version_to_save)
-
-        quant = update_project_files(cfg, version_to_save)
-
-        if cfg.commit:
-            files = {str(file.file) for file in cfg.files}
-            files.add(str(cfg.version_file))
-            vcs_handler.add_to_stage(files)
-            vcs_handler.create_commit(cfg.vcs_commit_message % {'version': version_to_save})
-
-    return quant
 
 
 def _find_project_config_file(user_config_file):
